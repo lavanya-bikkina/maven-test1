0e5abce42ad1a1905ed79a8de5c069a8c3db812f
lavanya-bikkina <bikkinadurgalavanya@gmail.com>
2025-08-17 16:47:33 +0530

    Delete src/main/java/com/example/javamavenjunithelloworld/Bikkina.java

--- src/main/java/com/example/javamavenjunithelloworld/Bikkina.java
+++
@@ -1,7 +0,0 @@
-package com.example.javamevanjunitehelloworld;
-
-public class Bikkina {
-    public static void main(String[] args) {
-        System.out.println("Hello, World from Bikkina!");
-    }
-}
