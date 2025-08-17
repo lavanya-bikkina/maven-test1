31a9965108046130e1e6324f58e59a0b95d7dfbf
Bikkina Durga Lavanya <bikkinadurgalavanya@gmail.com>
2025-08-17 16:35:26 +0530

    BIKKINAD0001 - Added my function and odd/even check

---
+++ src/main/java/com/example/javamavenjunithelloworld/Utility.py
@@ -0,0 +1,9 @@
+def bikkina():
+    print("I am Bikkina")
+
+# Example utility function: check if number is odd or even
+def is_odd_or_even(num: int) -> str:
+    if num % 2 == 0:
+        return "Even"
+    else:
+        return "Odd"
