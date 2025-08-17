331a9e5a64627ebbbe3ddbdd1f5d9e3b1442b429
Bikkina Durga Lavanya <bikkinadurgalavanya@gmail.com>
2025-08-17 15:36:54 +0530

    BIKKINAD0001: add Hello World program (Bikkina.java)

---
+++ src/main/java/com/example/javamavenjunithelloworld/Bikkina.java
@@ -0,0 +1,7 @@
+package com.example.javamevanjunitehelloworld;
+
+public class Bikkina {
+    public static void main(String[] args) {
+        System.out.println("Hello, World from Bikkina!");
+    }
+}
